b446652351bfb7aa73c855b544e0ad4e305f4f7a
vitalspace <myglobalsounds@gmail.com>
2024-05-16 19:05:50 -0600

    Modify guia instalacion url

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -51,7 +51,7 @@
   </a>
 
   <a
-    href="/guia"
+    href="/guia/instalacion"
     class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
   >
     <span class="text-[15px] ml-4 text-gray-200 font-bold">Instalacion</span>
