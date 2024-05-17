db7219af294f3ea5f14ba63cf9a40af7dde8769a
vitalspace <myglobalsounds@gmail.com>
2024-05-16 19:05:28 -0600

    Modify bass url repo

--- src/components/Guia/EditThisPage.svelte
+++ src/components/Guia/EditThisPage.svelte
@@ -7,7 +7,7 @@
 <div class="w-full flex flex-row justify-between place-items-center">
   <a target="_blank"
     class="text-[12px] text-purple-400 underline hover:text-purple-300 flex place-items-center space-x-2"
-    href={"https://github.com/ziglatino/zig-lat/blob/main/src/content" + url}
+    href={"https://github.com/ziglatino/zig-lat/blob/main/src/content" + url + ".mdx"}
     >
     <img class="h-4 w-4" src="/pencil.svg" alt="">
     <span>Edita esta pagina</span>
